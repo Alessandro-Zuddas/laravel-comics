83d9e1d31e965ce0343a0a379ac2e90bfdeb0a6a
Alessandro-Zuddas <alessandro.zuddas1999@gmail.com>
2023-01-15 09:35:01 +0100

    Aggiunge assets

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,54 @@
-<header>
-    <h1>Prova Header</h1>
+<header class="ms-header">
+
+    <div class="container-fluid ms-header-container">
+        <div class="container">
+            <div class="row flex-nowrap justify-content-end">
+                <small class="col-2 ms-header-t-text">DC POWER VISA@</small>
+                <small class="col-2 ms-header-t-text">ADDITIONAL SITES</small>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row flex-nowrap py-3">
+                <div class="col">
+                    <img src="../../img/dc-logo.png" alt="">
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+                <div class="col">
+                    <a href="#">Link</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
